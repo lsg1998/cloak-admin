4b9b000f33121c999d068af87b18f42cd0caafb6
lsg <lsg@lsg.com>
2025-10-02 01:08:10 +0800

    1111

--- admin-dist/assets/js/upload-C8ZmGt-u.js
+++
@@ -1,1 +0,0 @@
-import{a2 as o}from"./index-Dt7Dw0Fe.js";const t=a=>o.post("/upload/image",a,{cancel:!1}),p=a=>o.post("/upload/video",a,{cancel:!1});export{p as a,t as u};
--- admin-dist/assets/js/useAuthButtons-DtNg6gM8.js
+++
@@ -1,1 +0,0 @@
-import{A as s,ak as r,s as n}from"./index-Dt7Dw0Fe.js";const B=()=>{const u=s(),o=r().authButtonListGet[u.name]||[];return{BUTTONS:n(()=>{let t={};return o.forEach(e=>t[e]=!0),t})}};export{B as u};
