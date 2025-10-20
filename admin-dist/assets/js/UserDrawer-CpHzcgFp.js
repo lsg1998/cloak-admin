7674a0100667633e36d64a284718c9c80a2b3d63
lsg <lsg@lsg.com>
2025-10-20 19:43:56 +0800

    111

--- admin-dist/assets/js/UserDrawer-CpHzcgFp.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./UserDrawer.vue_vue_type_script_setup_true_name_UserDrawer_lang-X7-TSKSk.js";import"./index-CSDlNv8m.js";import"./Imgs--3XUe_eQ.js";import"./upload-BMBE57eP.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";export{o as default};
---
+++ admin-dist/assets/js/upload-DSMpaN8t.js
@@ -0,0 +1,1 @@
+import{a5 as o}from"./index-C9K-UNIA.js";const t=a=>o.post("/upload/image",a,{cancel:!1}),p=a=>o.post("/upload/video",a,{cancel:!1});export{p as a,t as u};
