77e08a969a3a83d4635955f42b4ab26fc3bbca39
lsg <lsg@lsg.com>
2026-08-03 17:51:55 +0800

    222

---
+++ admin-dist/assets/js/upload-qH6NCOdB.js
@@ -0,0 +1,1 @@
+import{Z as o}from"./index-CfHF4oEB.js";const s=a=>o.post("/admin/upload/image",a,{cancel:!1}),t=a=>o.post("/admin/upload/base64",a),n=a=>o.post("/admin/upload/image/convert-url",a,{cancel:!1}),l=a=>o.post("/admin/upload/video",a,{cancel:!1});export{l as a,t as b,n as c,s as u};
